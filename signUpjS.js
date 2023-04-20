a4cb37d2d345149a3334952afce9d36c92d23376
SRINJOY59 <119725096+SRINJOY59@users.noreply.github.com>
2023-04-20 22:46:51 +0530

    sign up page

---
+++ signUpjS.js
@@ -0,0 +1,28 @@
+let register = document.getElementById('getVal');
+function validateemail() {
+    let x = document.getElementById("user").value;
+    var atposition = x.indexOf('@');
+    var dotposition = x.lastIndexOf('.');
+    if (atposition < 1 || dotposition < atposition || dotposition + 1 === x.length) {
+      alert("Please enter a valid email address");
+      console.log(dotposition);
+      console.log(atposition);
+      return false;
+    } else {
+      alert("Email address found!");
+      return true;
+    }
+  }
+  
+register.addEventListener('click',(email)=>{
+    if(validateemail()){
+let password=document.getElementById('pass').value;
+let confirm = document.getElementById('confirmPass').value;
+      if(password!=confirm){
+        window.alert("Your password didn't match, try again");
+    }
+    else if(password===confirm){
+    document.getElementById('showpage').innerHTML = "Your Account have been registered";
+    }
+}
+})
