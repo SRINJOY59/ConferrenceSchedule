4849c414ff84ecec17d26053d236d15b77c4e19a
SRINJOY59 <119725096+SRINJOY59@users.noreply.github.com>
2023-04-21 02:59:50 +0530

    BlockChain Site Created

---
+++ BlockChain.js
@@ -0,0 +1,27 @@
+let loginBtn = document.getElementById('loginbtn');
+loginBtn.addEventListener('click',()=>{
+    let userName = document.getElementById('user').value;
+    let password = document.getElementById('pass').value;
+    function validateemail() {
+        console.log(userName);
+        var atposition = userName.indexOf('@');
+        var dotposition = userName.lastIndexOf('.');
+        if (atposition < 1 || dotposition < atposition || dotposition + 1 === userName.length) {
+            window.alert("Invalid username or password");
+          return false;
+        } else {
+          alert("Email address found!");
+          return true;
+        }
+      }
+    if(validateemail()){
+        if(password.length>=8){
+            window.alert("Registration Completed!!");
+        }
+    else{
+        window.alert("Invalid username or password");
+        userName='';
+        password='';
+    }
+}
+})
